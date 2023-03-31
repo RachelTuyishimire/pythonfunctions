73f624707a031b45587ed5332166c21354ee6d09
student <you@example.com>
2023-03-31 19:48:50 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,16 @@
+def add (a,b):
+    answer = a+b
+    return answer 
+    
+def subtract (x,y):
+    answer = x-y
+    return answer
+
+
+def multiply (m,n):
+    answer = m*n
+    return answer
+
+def remainder (c,d):
+    answer = c%d
+    return answer
